ed4dea64a838636fe19c8efc62cdce43baf2bdab
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-05-08 19:37:10 -0500

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thinojosa_inclass01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                /*
+                 * in-class 01
+                 * By: Tyler Hinojosa
+                 */
+                  
+                 // Display "Hello, World!" to a console
+                 Console.WriteLine("Hello, World!");
+
+                // Add a blank line
+                Console.WriteLine();
+
+            // Ask the user to push a key
+            // Wait for key press
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
